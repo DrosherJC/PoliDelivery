7ed137a2cf93e5cf5ab691c1fc7545e553f6888c
José Buñay <jose.bunay@epn.edu.ec>
2026-02-03 10:30:52 -0500

    Agregar validaciones en usuarios

--- usuarios.py
+++ usuarios.py
@@ -1,6 +1,10 @@
 import os
 
 ARCHIVO_USUARIOS = "data/usuarios.txt" #lugar donde se creara el archivo
+CODIGO_ADMIN = "e2tru25b"
+
+MAX_CARACTERES_NOMBRE = 50
+MAX_CARACTERES_EMAIL = 50
 
 def validar_contrasena(password):
     # Requisitos de contraseña: Minúsculas, 1 mayúscula, 1 número
@@ -17,11 +21,46 @@ def validar_contrasena(password):
 
 def registrar_usuario():
     print("\n--- REGISTRO DE USUARIO ---")
-    nombre = input("Nombre y Apellido: ")
-    identificacion = input("Identificación: ")
+    while True:
+        nombre = input(f"Nombre y Apellido (máx {MAX_CARACTERES_NOMBRE}): ")
+        if len(nombre) > MAX_CARACTERES_NOMBRE:
+            print(f"Error: El nombre es muy largo. Máximo {MAX_CARACTERES_NOMBRE} caracteres.")
+        elif len(nombre) < 1:
+            print("Error: El nombre no puede estar vacío.")
+        else:
+            break # El nombre es válido
+
+    #Validación de Identificación 
+    while True:
+        identificacion = input("Identificación (10 dígitos): ")
+        if len(identificacion) == 10 and identificacion.isdigit():
+            break # Es válido
+        else:
+            print("Error: La identificación debe tener exactamente 10 números y sin letras.")
+
     edad = input("Edad: ")
-    email = input("Usuario (email): ")
-    
+
+    #Validación de Email 
+    while True:
+        email = input(f"Usuario (email) (máx {MAX_CARACTERES_EMAIL}): ")
+        
+        if len(email) > MAX_CARACTERES_EMAIL:
+            print(f"Error: El correo es muy largo. Máximo {MAX_CARACTERES_EMAIL} caracteres.")
+        elif len(email) < 5:
+            print("Error: Correo demasiado corto.")
+        elif "@" not in email:
+            print("Error: El correo electrónico debe contener una arroba (@).")
+        else:
+            break #Es válido
+
+    if "admin" in email:
+        print("AVISO: Se ha detectado un intento de registro como ADMINISTRADOR.")
+        codigo_ingresado = input("Ingrese el código de autorización: ")
+        
+        if codigo_ingresado != CODIGO_ADMIN:
+            print("Error: Código de autorización incorrecto. Registro cancelado.")
+            return 
+
     while True:
         password = input("Contraseña: ")
         if validar_contrasena(password):
@@ -29,7 +68,6 @@ def registrar_usuario():
         else:
             print("Error: La contraseña debe tener al menos una mayúscula y un número.")
             
-    # Guardar en archivo
     try:
         with open(ARCHIVO_USUARIOS, "a", encoding="utf-8") as f:
             linea = f"{email},{password},{nombre},{identificacion},{edad}\n"
@@ -54,7 +92,7 @@ def iniciar_sesion():
                 if len(datos) >= 5:
                     email, password, nombre, _, _ = datos
                     if email == email_input and password == pass_input:
-                        # Retorna el nombre y el rol (simple lógica para demo)
+                        # Retorna el nombre y el rol 
                         rol = "admin" if "admin" in email else "cliente"
                         return nombre, rol
         print("Credenciales incorrectas.")
