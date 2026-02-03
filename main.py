73058d428f3e50376de1acd4f7bde32a22b04159
morenojacomedanna-sys <morenojacomedanna@gmail.com>
2026-02-03 00:40:06 -0500

    Agregando contenido carpeta main

---
+++ main.py
@@ -0,0 +1,8 @@
+import menu
+
+def main():
+    print("Iniciando Sistema PoliDelivery...")
+    # aqui se llama a la función iniciar del módulo menu, IMP-recordar.
+    menu.iniciar_sistema()
+
+main()
